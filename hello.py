70d0fd37cba12d00022693eebe83ef90c312faca
Ashishkh2623 <aaashish4341@gmail.com>
2026-01-08 15:03:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello from my CI/CD pipeline!")
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CI/CD Project</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>CI/CD Pipeline using GitHub Actions</h1>
+    <p>This website is deployed automatically.</p>
+</body>
+</html>
